43524c1683e7d20548443b4fb3e9e56052af2ca7
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-13 10:20:28 +0200

    Do the Javadoc for class DBManager.

--- src/main/java/mainTER/DBManage/DBManager.java
+++ src/main/java/mainTER/DBManage/DBManager.java
@@ -7,18 +7,34 @@ public class DBManager {
     private Connection connection = null;
     private final String nameDB;
 
+    /**
+     * Constructor who take the database name for the project.
+     */
     public DBManager(){
         this("DatabasesProject");
     }
 
+    /**
+     * Constructor who take one database name for test principally (the file with this name must exist).
+     * @param nameDB name for the database.
+     */
     public DBManager(String nameDB){
+        // TODO Create the file with the name nameDB.db if it doesn't exist anymore.
         this.nameDB = nameDB;
     }
 
+    /**
+     * Connect to the databases (which is a file in this case).
+     * @throws SQLException when the file doesn't exist or if the user and/or the password is not good.
+     */
     private void getConnection() throws SQLException {
         connection = DriverManager.getConnection("jdbc:hsqldb:file:"+nameDB, "SA", "");
     }
 
+    /**
+     * Function who create a table or insert values into a table.
+     * @param strCreateTable request for the databases (must be create table or select).
+     */
     public void createTableOrInsert(String strCreateTable){
         try {
             this.getConnection();
@@ -33,6 +49,10 @@ public class DBManager {
         }
     }
 
+    /**
+     * Function who drop a table.
+     * @param strDropTable name of the table we want to delete.
+     */
     public void dropTable(String strDropTable){
         try{
             this.getConnection();
@@ -50,6 +70,11 @@ public class DBManager {
         }
     }
 
+    /**
+     * Do a request for a select into the databases.
+     * @param strSelect Request for a select in the table
+     * @return a ResultSet who contains all result for the request.
+     */
     public ResultSet selectIntoTable(String strSelect){
         try{
             this.getConnection();
@@ -65,6 +90,9 @@ public class DBManager {
         return null;
     }
 
+    /**
+     * Drop all tables.
+     */
     public void dropCascade(){
         try {
             this.getConnection();
