69daefe8d658d0865219677df400216908dfffd8
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-14 12:10:24 +0200

    test use Title.fxml

--- src/main/java/mainTER/Menu/Title.java
+++ src/main/java/mainTER/Menu/Title.java
@@ -1,29 +1,33 @@
 package mainTER.Menu;
 
+import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-
+import mainTER.LoadOfFXML;
 
 
 public class Title extends StackPane {
+
+    @FXML
+    private Text textTitle;
 
     /**
      * Constructor of the title
      * @param name is the title name
      */
     public Title(String name){
-
-
-        Text text = new Text(name);
+        LoadOfFXML.loadFXML("/mainTER/Menu/FXML/Title.fxml", this, this);
+        textTitle.setText(name);
+        /*Text text = new Text(name);
         text.setFill(Color.WHITE);
         text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD,40));
 
 
         setAlignment(Pos.TOP_CENTER);
-        getChildren().addAll(text);
+        getChildren().addAll(text);*/
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module mainTER {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
     requires hsqldb;
     requires java.sql;
+    requires java.desktop;
     exports mainTER;
     opens mainTER.Menu;
 }
