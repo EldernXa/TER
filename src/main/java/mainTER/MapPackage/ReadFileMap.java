d53c69a797839b2a5a2fd7dcd0969494c82b5b08
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-04-15 17:41:08 +0200

    Amélioration du readfile en le rendant plus modulable

--- src/main/java/mainTER/MapPackage/ReadFileMap.java
+++ src/main/java/mainTER/MapPackage/ReadFileMap.java
@@ -35,85 +35,60 @@ public class ReadFileMap {
 
     private void read() {
 
-        String lastCategorie;
+        String lastCategorie = "";
         int i = 0;
-
-
+        String line[];
         MapFieldFromSprite fieldFromSprite;
         MapFieldFromLilPict fieldFromLilPict;
-        try {
 
-            String line[] = file[i].split("\\s+");
+        while(i < file.length){
+            line = file[i].split("\\s+");
 
-
-            if (line[0].equals("floor")) {
-
-                i++;
-                line = file[i].split("\\s+");
-                while (!line[0].equals("objects")) {
-
-
-
-
-                    spriteName = line[0];
-
-                    double[] doubles = new double[line.length];
-                    for (int j = 2; j < line.length; j++) {
-
-                        doubles[j] = Integer.parseInt(line[j]);
-
-                    }
-
-
-                    if (line[1].equals("L")) {
-
-                        fieldFromLilPict = new MapFieldFromLilPict("./src/main/resources/mainTER/MapPackage/Sprites/Front/" + spriteName,
-                                new Coordinate(doubles[2], doubles[3]), doubles[4], doubles[5]);
-                        mapFieldFormArrayList.add(fieldFromLilPict);
-
-                    } else {
-
-                        fieldFromSprite = new MapFieldFromSprite("./src/main/resources/mainTER/MapPackage/Sprites/Front/" + spriteName,
-                                new Coordinate(doubles[2], doubles[3]), 100);
-                        mapFieldFormArrayList.add(fieldFromSprite);
-
-
-                    }
-
+            switch (line[0]){//add case if you add section
+                case "floor":
+                    lastCategorie = "floor";
                     i++;
-                    line = file[i].split("\\s+");
+                    break;
+                case "objects":
+                    lastCategorie = "objects";
+                    i++;
+                    break;
+                default:
+                    if(lastCategorie.equals("floor")){
+                        line = file[i].split("\\s+");
+                        spriteName = line[0];
+                        double[] doubles = new double[line.length];
 
+                        for (int j = 2; j < line.length; j++) {
+                            doubles[j] = Integer.parseInt(line[j]);
+                        }
 
-
-                }
-
-
-            }
-
-            if (line[0].equals("objects")) {
-                i++;
-                while (i < file.length) {
-                    line = file[i].split("\\s+");
-
-                    double[] doubles = new double[line.length];
-                    for (int j = 1; j < line.length; j++) {
-                        doubles[j] = Integer.parseInt(line[j]);
-
+                        if (line[1].equals("L")) {
+                            fieldFromLilPict = new MapFieldFromLilPict("./src/main/resources/mainTER/MapPackage/Sprites/Front/" + spriteName, new Coordinate(doubles[2], doubles[3]), doubles[4], doubles[5]);
+                            mapFieldFormArrayList.add(fieldFromLilPict);
+                        }
+                        else {
+                            fieldFromSprite = new MapFieldFromSprite("./src/main/resources/mainTER/MapPackage/Sprites/Front/" + spriteName, new Coordinate(doubles[2], doubles[3]), 100);
+                            mapFieldFormArrayList.add(fieldFromSprite);
+                        }
                     }
-                    switch (line[0]) {//Add case when new object
-                        case "crate":
-                            interactiveObjectArrayList.add(new Crate(new Coordinate(doubles[1], doubles[2])));
+                    else if(lastCategorie.equals("objects")){
+                        line = file[i].split("\\s+");
 
+                        double[] doubles = new double[line.length];
+                        for (int j = 1; j < line.length; j++) {
+                            doubles[j] = Integer.parseInt(line[j]);
+
+                        }
+                        switch (line[0]) {//Add case when new object
+                            case "crate":
+                                interactiveObjectArrayList.add(new Crate(new Coordinate(doubles[1], doubles[2])));
+                        }
+                    }
+                    else{
                     }
                     i++;
-                }
-
-
             }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
