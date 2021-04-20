8a87389d864a0683f954e2dc5a36d7e84b2a1fd8
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-20 10:58:28 +0200

    remove unused import in Character class.

--- src/main/java/mainTER/CharacterGameplay/Character.java
+++ src/main/java/mainTER/CharacterGameplay/Character.java
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.logging.Logger;
 
 import mainTER.Tools.Coordinate;
 import mainTER.exception.CharacterImageFileDoesntExist;
