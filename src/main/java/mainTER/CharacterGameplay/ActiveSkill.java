da5593e020465fb8b950188d56a323bb74335076
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-22 11:27:52 +0200

    add more attribute.

--- src/main/java/mainTER/CharacterGameplay/ActiveSkill.java
+++ src/main/java/mainTER/CharacterGameplay/ActiveSkill.java
@@ -5,16 +5,22 @@ import javafx.scene.input.KeyEvent;
 
 public class ActiveSkill {
     private final String ctrlKey;
-    private final boolean isAnimate;
+    private final boolean animateMvt;
+    private final boolean animateAction;
+    private final boolean isMode;
     private final String nameSkill;
     private final Characteristics characteristics;
     private final ActiveSkillEnum skill;
+    private boolean isEnabled;
 
-    public ActiveSkill(String nameSkill, String ctrlKey, boolean isAnimate, Characteristics characteristics){
+    public ActiveSkill(String nameSkill, String ctrlKey, boolean animateMvt, boolean animateAction, boolean isMode, Characteristics characteristics){
         this.nameSkill = nameSkill;
         this.ctrlKey = ctrlKey;
-        this.isAnimate = isAnimate;
+        this.animateMvt = animateMvt;
+        this.animateAction = animateAction;
+        this.isMode = isMode;
         this.characteristics = characteristics;
+        isEnabled = false;
         skill = ActiveSkillEnum.valueOf(this.nameSkill);
     }
 
@@ -22,7 +28,11 @@ public class ActiveSkill {
         return event -> {
             if (event.getCode().getChar().equals(ctrlKey)){
                 if(skill == ActiveSkillEnum.SHIELD_MODE){
-                    characteristics.setCanJump(false);
+                    if(!isEnabled) {
+                        characteristics.setCanJump(false);
+                    }else{
+                        characteristics.setCanJump(true);
+                    }
                 }
             }
         };
