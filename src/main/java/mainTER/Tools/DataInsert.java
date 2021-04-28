43715cd4863006bfe3501bd1633252d89efcef50
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-28 10:25:15 +0200

    not inserting in db all the time now.

--- src/main/java/mainTER/Tools/DataInsert.java
+++ src/main/java/mainTER/Tools/DataInsert.java
@@ -7,23 +7,28 @@ import mainTER.exception.*;
 
 public class DataInsert {
 
+    private static final int NB_CHARACTER = 4;
+    private static final int NB_SKILL = 5;
+
     private DataInsert(){
 
     }
 
     public static void insertPerson(){
         PersonDBManager personDBManager = new PersonDBManager();
-        personDBManager.removeTablePerson();
-        personDBManager.createTablePerson();
-        try {
-            personDBManager.insertIntoTablePerson("Paladin", 5, 20, 5, 5, true);
-            personDBManager.insertIntoTablePerson("Demon", 10, 5, 8, 2, true);
-            personDBManager.insertIntoTablePerson("Serpent", 8, 10, 0, 3, false);
-            personDBManager.insertIntoTablePerson("HommeDragon", 10, 5, 8, 2, true);
-        }catch(PersonDataAlreadyExistException personDataAlreadyExistException){
-            System.out.println("Problème dans l'insertion des données des Personnages.");
-        }catch(PersonDataDoesntCorrectException personDataDoesntCorrectException){
-            System.out.println("Les données inséres ne sont pas correcte.");
+        if(personDBManager.getListNameFromDatabase().size()!=NB_CHARACTER) {
+            personDBManager.removeTablePerson();
+            personDBManager.createTablePerson();
+            try {
+                personDBManager.insertIntoTablePerson("Paladin", 5, 20, 5, 5, true);
+                personDBManager.insertIntoTablePerson("Demon", 10, 5, 8, 2, true);
+                personDBManager.insertIntoTablePerson("Serpent", 8, 10, 0, 3, false);
+                personDBManager.insertIntoTablePerson("HommeDragon", 10, 5, 8, 2, true);
+            } catch (PersonDataAlreadyExistException personDataAlreadyExistException) {
+                System.out.println("Problème dans l'insertion des données des Personnages.");
+            } catch (PersonDataDoesntCorrectException personDataDoesntCorrectException) {
+                System.out.println("Les données inséres ne sont pas correcte.");
+            }
         }
     }
 
@@ -40,17 +45,19 @@ public class DataInsert {
 
     public static void insertSkill(){
         SkillDBManager skillDBManager = new SkillDBManager();
-        skillDBManager.removeTableSkill();
-        skillDBManager.createTableSkill();
+        if(skillDBManager.getListSkillName().size() != NB_SKILL) {
+            skillDBManager.removeTableSkill();
+            skillDBManager.createTableSkill();
 
-        try {
-            skillDBManager.insertIntoTableSkill("SHIELD", "1", "Paladin", true, false, true);
-            skillDBManager.insertIntoTableSkill("ATTACK", "2", "Paladin", false, true, false);
-            skillDBManager.insertIntoTableSkill("BARRIER", "3", "Paladin", true, false, true);
-            skillDBManager.insertIntoTableSkill("FLY", "1", "Demon", true, false, true);
-            skillDBManager.insertIntoTableSkill("MOULT", "1", "Serpent", false, false, false);
-        }catch(SkillAlreadyExistException | SkillCtrlAlreadyUsedException | SkillDataDoesntCorrectException exception){
-            System.out.println(exception.getMessage());
+            try {
+                skillDBManager.insertIntoTableSkill("SHIELD", "1", "Paladin", true, false, true);
+                skillDBManager.insertIntoTableSkill("ATTACK", "2", "Paladin", false, true, false);
+                skillDBManager.insertIntoTableSkill("BARRIER", "3", "Paladin", true, false, true);
+                skillDBManager.insertIntoTableSkill("FLY", "1", "Demon", true, false, true);
+                skillDBManager.insertIntoTableSkill("MOULT", "1", "Serpent", false, false, false);
+            } catch (SkillAlreadyExistException | SkillCtrlAlreadyUsedException | SkillDataDoesntCorrectException exception) {
+                System.out.println(exception.getMessage());
+            }
         }
     }
 
