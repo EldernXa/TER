c2642c3fde14ef6e3d065fe5520c0869c019faa8
LEMAIRE <anthony_pro1998@outlook.fr>
2021-04-30 15:18:32 +0200

    Add Test for Coordinate.

---
+++ src/test/java/mainTER/Tools/TestCoordinate.java
@@ -0,0 +1,36 @@
+package mainTER.Tools;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestCoordinate {
+
+    private Coordinate coordinate;
+    private final double coordinateX = 50;
+    private final double coordinateY = 63;
+    private final int step = 50;
+
+    @BeforeEach
+    void initTest(){
+        coordinate = new Coordinate(coordinateX, coordinateY);
+    }
+
+    @Test
+    void testGettingCoordinate(){
+        assertEquals(coordinateX, coordinate.getX());
+        assertEquals(coordinateY, coordinate.getY());
+    }
+
+    @Test
+    void testGettingBeforeAndAfterSettingForCoordinate(){
+        assertEquals(coordinateX, coordinate.getX());
+        assertEquals(coordinateY, coordinate.getY());
+        coordinate.setX(coordinateX+step);
+        coordinate.setY(coordinateY+step);
+        assertEquals(coordinateX+step, coordinate.getX());
+        assertEquals(coordinateY+step, coordinate.getY());
+    }
+
+}
