f47e25d284513cf3cf1e34f9695f96bd767c7390
LEMAIRE <anthony_pro1998@outlook.fr>
2021-05-06 10:06:58 +0200

    add Javadoc.

--- src/main/java/mainTER/DBManage/MapDBManager.java
+++ src/main/java/mainTER/DBManage/MapDBManager.java
@@ -9,24 +9,43 @@ import java.sql.SQLException;
 
 public class MapDBManager {
 
+    /**
+     * variable to connect to the databases.
+     */
     private final DBManager dbManager;
 
+    /**
+     * Variable who says if the instance is used for test or for the app.
+     */
     private final boolean isForTest;
 
+    /**
+     * The name of the databases.
+     */
     private final String nameDatabases;
 
+    /**
+     * Constructor for the App.
+     */
     public MapDBManager(){
         this.dbManager = new DBManager();
         isForTest = false;
         nameDatabases = "";
     }
 
+    /**
+     * Constructor for the test.
+     * @param name name of the databases for the test.
+     */
     public MapDBManager(String name){
         this.dbManager = new DBManager(name, "test");
         isForTest = true;
         nameDatabases = name;
     }
 
+    /**
+     * Create the table Map in the databases.
+     */
     public void createTableMap(){
         dbManager.createTableOrInsert("CREATE TABLE Map (" +
                 "mapName VARCHAR(50) PRIMARY KEY," +
@@ -36,6 +55,14 @@ public class MapDBManager {
                 ");");
     }
 
+    /**
+     * Insert values into the table Map in the databases.
+     * @param mapName the name of the map we want to insert.
+     * @param nameFirstCharacter the name of the first character we want to insert.
+     * @param coordinateX the x coordinate of the initial position in the map.
+     * @param coordinateY the y coordinate of the initial position in the map.
+     * @throws MapCharacterNotExistException if the first character doesn't exist in the databases.
+     */
     public void insertIntoTableMap(String mapName, String nameFirstCharacter, double coordinateX, double coordinateY)
             throws MapCharacterNotExistException{
         PersonDBManager personDBManager;
@@ -56,6 +83,12 @@ public class MapDBManager {
         dbManager.createTableOrInsert(reqValues);
     }
 
+    /**
+     * Getting the name of the first character thanks to the name of the map.
+     * @param mapName the name of the map.
+     * @return the name of the first character thanks to the name of the map.
+     * @throws MapDataGetException if the name given doesn't exist in the databases.
+     */
     public String getFirstCharacter(String mapName) throws MapDataGetException {
         ResultSet resultSet = selectMapIntoTableMap(mapName);
         try {
@@ -65,6 +98,12 @@ public class MapDBManager {
         }
     }
 
+    /**
+     * Getting the initial coordinate for the map we ask.
+     * @param mapName the name of the map.
+     * @return the initial coordinate for the map we ask.
+     * @throws MapDataGetException if the name given doesn't exist in the databases.
+     */
     public Coordinate getInitialCoordinate(String mapName) throws MapDataGetException{
         ResultSet resultSet = selectMapIntoTableMap(mapName);
         try {
@@ -75,6 +114,11 @@ public class MapDBManager {
         }
     }
 
+    /**
+     * Getting all data for a map.
+     * @param mapName the name of a map.
+     * @return all data for the map we ask.
+     */
     private ResultSet selectMapIntoTableMap(String mapName){
         ResultSet resultSet;
         resultSet = dbManager.selectIntoTable("SELECT * FROM Map WHERE mapName = '" +
@@ -87,6 +131,9 @@ public class MapDBManager {
         return resultSet;
     }
 
+    /**
+     * Remove the table Map.
+     */
     public void removeTableMap(){
         try{
             dbManager.dropTable("Map");
@@ -95,6 +142,9 @@ public class MapDBManager {
         }
     }
 
+    /**
+     * Drop cascade.
+     */
     public void dropCascade(){
         dbManager.dropCascade();
     }
