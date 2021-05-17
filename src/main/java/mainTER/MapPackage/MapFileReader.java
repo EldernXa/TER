ffaa586765899bb3bbe6ba66a6ba327565f9e8db
sykom1 <saber.tnifas@etu.univ-amu.fr>
2021-05-17 17:46:00 +0200

    différencie les collissions object entre multi et solo

--- src/main/java/mainTER/MapPackage/MapFileReader.java
+++ src/main/java/mainTER/MapPackage/MapFileReader.java
@@ -24,8 +24,8 @@ public class MapFileReader {
     /**
      * @param pathName Map file path
      */
-    public MapFileReader(String url, String pathName) {
-        //if(detectableObjectArrayList == null){
+    public MapFileReader(String url, String pathName,boolean bool) {
+        if(bool){
             checkpointArrayList = new ArrayList<>();
             detectableObjectArrayList = new ArrayList<>();
             this.pathName = pathName;
@@ -38,10 +38,25 @@ public class MapFileReader {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-      //  }
+        }else {
+            if(detectableObjectArrayList == null){
+                checkpointArrayList = new ArrayList<>();
+                detectableObjectArrayList = new ArrayList<>();
+                this.pathName = pathName;
+                Path path = Paths.get(url + pathName + ".txt");
+                try {
+                    this.file = Files.readString(path).split("\n");
+
+                    read();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
 
     }
-
 
 
     /**
