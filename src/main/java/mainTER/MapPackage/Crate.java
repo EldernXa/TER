77967270fe5f3c0dd889d95b9e4f1f8ba1f61fbb
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-05-17 12:59:37 +0200

    add Javadoc Crate

--- src/main/java/mainTER/MapPackage/Crate.java
+++ src/main/java/mainTER/MapPackage/Crate.java
@@ -4,12 +4,23 @@ import javafx.scene.Node;
 import mainTER.Tools.Coordinate;
 import mainTER.Tools.ImageViewSizePos;
 
-public class Crate extends InteractiveObject { //TODO make it a CollideObject not an InteractiveObject
+/**
+ * Class representing a Crate, it position, it appearance, it interaction and lot more
+ */
+public class Crate extends InteractiveObject {
 
+    /**
+     * Class constructor
+     * @param coordinate
+     */
     public Crate(Coordinate coordinate ) {
         super(coordinate, new ImageViewSizePos("./src/main/resources/mainTER/MapPackage/Objects/crate.png",coordinate));
     }
 
+    /**
+     * Specifying the way the object react to a collide with an other DetectableObject
+     * @param detectableObject
+     */
     public void interaction(DetectableObject detectableObject) {
         if(detectableObject.getY() + detectableObject.getHeight() >= super.getY() + super.getHeight()*2/5) {
             if (detectableObject.getCoordinate().getX() < super.getCoordinate().getX()) {
@@ -20,31 +31,55 @@ public class Crate extends InteractiveObject { //TODO make it a CollideObject no
         }
     }
 
+    /**
+     * Return the appropriate node for a Crate (the imageView corresponding to it appearance)
+     * @return
+     */
     @Override
     public Node getAppropriateNode() {
         return super.getImageView();
     }
 
+    /**
+     * Return a clone of the Crate that is totally identical but doesn't occupy the same memory location
+     * @return
+     */
     @Override
     public Crate clone() {
         return new Crate(new Coordinate(this.getX(),this.getY()));
     }
 
+    /**
+     * Return the horizontal mouvement span for a Crate that is 10
+     * @return
+     */
     @Override
     public double getHMouvementSpan() {
         return 10;
     }
 
+    /**
+     * Return the appropriate jump mouvement span for a Crate that is 0
+     * @return
+     */
     @Override
     public double getJumpMouvementSpan() {
         return 0;
     }
 
+    /**
+     * Return the Crate coordinate
+     * @return
+     */
     @Override
     public Coordinate getCoordinate() {
         return super.getCoordinate();
     }
 
+    /**
+     * Set the Crate coordinate with the value @param
+     * @param coordinate
+     */
     @Override
     public void setCoordinate(Coordinate coordinate) {
         super.setCoordinate(coordinate);
