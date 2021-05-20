ca459d1d2589d7a1105e2c4a1637a27773608d30
anthony <anthony_pro1998@outlook.fr>
2021-05-20 18:16:45 +0200

    fix order animation.

--- src/main/java/mainTER/CharacterGameplay/AnimationCharacter.java
+++ src/main/java/mainTER/CharacterGameplay/AnimationCharacter.java
@@ -98,13 +98,16 @@ public class AnimationCharacter {
                 setReverseMotionLess();
             }
         }
-        indImgToAnimate++;
+        indImgToAnimate = (indImgToAnimate+1) % listOfImageViewForTheAnimation.get(posToAnimate).size();
         return imgView;
     }
 
     public ImageView actualImg(){
         try {
-            return listOfImageViewForTheAnimation.get(posToAnimate).get((indImgToAnimate - 1) % listOfImageViewForTheAnimation.get(posToAnimate).size());
+            int indice = (indImgToAnimate - 1) % listOfImageViewForTheAnimation.get(posToAnimate).size();
+            if(indice==-1)
+                indice = 0;
+            return listOfImageViewForTheAnimation.get(posToAnimate).get(indice);
         }catch(ArithmeticException arithmeticException){
             return listOfImageViewForTheAnimation.get(posToAnimate).get((indImgToAnimate));
         }
