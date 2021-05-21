ef2e6a5ca26320aaa053411dc4403ffd04fc11c4
farouk2101 <farouk.kedadi@etu.univ-amu.fr>
2021-05-21 15:01:22 +0200

    add JavaDOc ObjectLinker

--- src/main/java/mainTER/MapPackage/ObjectLinker.java
+++ src/main/java/mainTER/MapPackage/ObjectLinker.java
@@ -5,6 +5,9 @@ import mainTER.Tools.Coordinate;
 
 import javax.swing.plaf.synth.SynthOptionPaneUI;
 
+/**
+ * Class usefull to link 2 items together so when something happens to one of both it happens to the second one too
+ */
 public class ObjectLinker {
 
     DetectableObject detectableObject1;
@@ -15,18 +18,34 @@ public class ObjectLinker {
         this.detectableObject2 = detectableObject2;
     }
 
+    /**
+     * Get the first DetectableObject
+     * @return
+     */
     public DetectableObject getCollideObject1() {
         return detectableObject1;
     }
 
+    /**
+     * Get the second DetectableObject
+     * @return
+     */
     public DetectableObject getCollideObject2() {
         return detectableObject2;
     }
 
+    /**
+     * Set the First DetectableObject
+     * @param detectableObject1
+     */
     public void setCollideObject1(DetectableObject detectableObject1) {
         this.detectableObject1 = detectableObject1;
     }
 
+    /**
+     * Set the second DetectableObject
+     * @param detectableObject2
+     */
     public void setCollideObject2(DetectableObject detectableObject2) {
         this.detectableObject2 = detectableObject2;
     }
