b833ecd3da53b740bc3db7cf42467b67cd6dbdbc
anthony <anthony_pro1998@outlook.fr>
2021-05-22 15:18:01 +0200

    finish Javadoc for UpgradeSkill db.

--- src/main/java/mainTER/DBManage/UpgradeSkillDBManager.java
+++ src/main/java/mainTER/DBManage/UpgradeSkillDBManager.java
@@ -28,14 +28,24 @@ public class UpgradeSkillDBManager {
 
     private final DBManager dbManager;
 
+    /**
+     * Constructor for the application.
+     */
     public UpgradeSkillDBManager(){
         this.dbManager = new DBManager();
     }
 
+    /**
+     * Constructor for the test purpose.
+     * @param name the name of the databases.
+     */
     public UpgradeSkillDBManager(String name){
         this.dbManager = new DBManager(name, "test");
     }
 
+    /**
+     * Creating the table UpgradeSkill.
+     */
     public void createTableUpgradeSkill(){
         ArrayList<String> listName = new ArrayList<>();
         ArrayList<Integer> listSize = new ArrayList<>();
@@ -50,6 +60,10 @@ public class UpgradeSkillDBManager {
         dbManager.createTable(TABLE_NAME, listName, 4, listSize);
     }
 
+    /**
+     * verify if table UpgradeSkill exist.
+     * @return true if table UpgradeSkill exist, false otherwise.
+     */
     public boolean verifyTableUpgradeSkillExist(){
         ResultSet resultSet = dbManager.selectIntoTable("SELECT * FROM UpgradeSkill");
         try{
@@ -62,6 +76,15 @@ public class UpgradeSkillDBManager {
         return false;
     }
 
+    /**
+     * insert values into the table UpgradeSkill
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @param newValue the new value.
+     * @param price the price of the upgrade.
+     * @param description the description of the upgrade.
+     */
     public void insertIntoTableUpgradeSkill(String nameCharacter, int numSkill, String nameUpgrade, float newValue,
                                             int price, String description){
         ArrayList<Object> listObject = new ArrayList<>();
@@ -76,6 +99,12 @@ public class UpgradeSkillDBManager {
         dbManager.insertIntoTable(TABLE_NAME, listObject);
     }
 
+    /**
+     * Get list of upgrade.
+     * @param nameCharacter the name of the character we want to get the upgrades.
+     * @return a map who contains all the num skill and the name upgrade for a character.
+     * @throws UpgradeSkillDataGetException if the character doesn't contains upgrade.
+     */
     public Map<Integer, String> getListUpgradeSkillOfACharacter(String nameCharacter) throws UpgradeSkillDataGetException{
         HashMap<Integer, String> listUpgradeSkill = new HashMap<>();
         ResultSet resultSet = selectUpgradeSkillForACharacter(nameCharacter);
@@ -92,6 +121,13 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * get list of upgrade.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @return a list who contains a upgrade name for a character.
+     * @throws UpgradeSkillDataGetException if the character doesn't contains any upgrade for this skill.
+     */
     public List<String> getListUpgradeOfASkillOfACharacter(String nameCharacter, int numSkill) throws UpgradeSkillDataGetException{
         ArrayList<String> listNameLine = new ArrayList<>();
         ArrayList<Object> listRealValue = new ArrayList<>();
@@ -108,6 +144,13 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get list Upgrade sorted.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @return a list who contains a upgrade name for a character.
+     * @throws UpgradeSkillDataGetException if the character doesn't contains any upgrade for this skill.
+     */
     public List<String> getListUpgrade(String nameCharacter, int numSkill) throws UpgradeSkillDataGetException {
         ArrayList<String> listReturn = new ArrayList<>();
         ResultSet resultSet = selectUpgradeSkillForACharacter(nameCharacter);
@@ -125,6 +168,13 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get the last upgrade available for a name upgrade and a skill for the constructor principally.
+     * @param nameCharacter the name of a character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return the last upgrade available for a name upgrade and a skill.
+     */
     private Integer getLastNum(String nameCharacter, int numSkill, String nameUpgrade){
         int num = 0;
         ResultSet resultSet = selectUpgradeSkillForACharacter(nameCharacter);
@@ -144,6 +194,13 @@ public class UpgradeSkillDBManager {
         return num;
     }
 
+    /**
+     * Get the last upgrade available for a name upgrade and a skill for the application purpose.
+     * @param nameCharacter the name of a character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return the last upgrade available for a name upgrade and a skill and -1 if there aren't upgrade skill.
+     */
     public Integer getLastNumOfAUpgrade(String nameCharacter, int numSkill, String nameUpgrade){
         int num = getLastNum(nameCharacter, numSkill, nameUpgrade);
         if(num==0)
@@ -151,6 +208,15 @@ public class UpgradeSkillDBManager {
         return num-1;
     }
 
+    /**
+     * Get values from the table UpgradeSkill.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @param stringRequested the name of the attribute.
+     * @return a values from the table UpgradeSkill.
+     * @throws SQLException if the upgrade requested doesn't exist.
+     */
     private String getObject(String nameCharacter, int numSkill, String nameUpgrade, String stringRequested) throws SQLException {
         List<String> listName = listNameForGetting();
         ArrayList<Object> listRequest = new ArrayList<>();
@@ -160,6 +226,14 @@ public class UpgradeSkillDBManager {
         return dbManager.getData(TABLE_NAME, listName, listRequest, stringRequested);
     }
 
+    /**
+     * Get the num of an upgrade.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return the num of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public Integer getNumUpgrade(String nameCharacter, int numSkill, String nameUpgrade) throws UpgradeSkillDataGetException{
         try{
             return Integer.parseInt(getObject(nameCharacter, numSkill, nameUpgrade, ATTRIBUTE_STRING_NUM_UPGRADE));
@@ -168,6 +242,14 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get the new value of an upgrade.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return the num of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public float getNewValue(String nameCharacter, int numSkill, String nameUpgrade) throws UpgradeSkillDataGetException{
         try{
             return Float.parseFloat(getObject(nameCharacter, numSkill, nameUpgrade, ATTRIBUTE_STRING_NEW_VALUE));
@@ -176,6 +258,16 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * get value with num upgrade in addition.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @param numUpgrade the num of the upgrade.
+     * @param stringRequested the name of the attribute we want.
+     * @return a value.
+     * @throws SQLException if the upgrade requested doesn't exist.
+     */
     public String getObjectWithNumUpgrade(String nameCharacter, int numSkill, String nameUpgrade, int numUpgrade, String stringRequested) throws SQLException {
         List<String> listName = listNameForGetting();
         listName.add(ATTRIBUTE_STRING_NUM_UPGRADE);
@@ -187,6 +279,15 @@ public class UpgradeSkillDBManager {
         return dbManager.getData(TABLE_NAME, listName, listRequest, stringRequested);
     }
 
+    /**
+     * get the new value of an upgrade with num upgrade in addition.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @param numUpgrade the num of an upgrade.
+     * @return the new value of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public float getNewValueWithNumUpgrade(String nameCharacter, int numSkill, String nameUpgrade, int numUpgrade) throws UpgradeSkillDataGetException{
         try{
             return Float.parseFloat(getObjectWithNumUpgrade(nameCharacter, numSkill, nameUpgrade, numUpgrade, ATTRIBUTE_STRING_NEW_VALUE));
@@ -195,6 +296,12 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Set that the upgrade is already learned.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     */
     public void setUpgradeDone(String nameCharacter, int numSkill, String nameUpgrade){
         String request = "UPDATE UpgradeSkill SET isAlreadyLearned = '" + SecureManage.getEncrypted("true")+"'" +
                 " WHERE nameCharacter = '" + SecureManage.getEncrypted(nameCharacter) + "' AND numSkill = '" + SecureManage.getEncrypted(String.valueOf(numSkill))
@@ -202,6 +309,14 @@ public class UpgradeSkillDBManager {
         dbManager.updateTable(request);
     }
 
+    /**
+     * Get if the upgrade is already learned.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return true if the upgrade is already learned, false otherwise.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public boolean getIsAlreadyDone(String nameCharacter, int numSkill, String nameUpgrade) throws UpgradeSkillDataGetException{
         try{
             return getObject(nameCharacter, numSkill, nameUpgrade, ATTRIBUTE_STRING_IS_ALREADY_LEARNED).compareTo("true") == 0;
@@ -210,6 +325,15 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get isAlreadyLearned with num upgrade in addition.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @param numUpgrade the num of the upgrade.
+     * @return true if the upgrade is already learned, false otherwise.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public boolean getIsAlreadyDoneWithNumUpgrade(String nameCharacter, int numSkill, String nameUpgrade, int numUpgrade) throws UpgradeSkillDataGetException{
         try{
             return getObjectWithNumUpgrade(nameCharacter, numSkill, nameUpgrade, numUpgrade, ATTRIBUTE_STRING_IS_ALREADY_LEARNED).compareTo("true") != 0;
@@ -218,6 +342,14 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get price.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @return the price of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public int getPrice(String nameCharacter, int numSkill, String nameUpgrade) throws UpgradeSkillDataGetException{
         try{
             return Integer.parseInt(getObject(nameCharacter, numSkill, nameUpgrade, ATTRIBUTE_STRING_PRICE));
@@ -226,6 +358,15 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get price with num upgrade in addition.
+     * @param nameCharacter the name of the character.
+     * @param numSkill the num of the skill.
+     * @param nameUpgrade the name of the upgrade.
+     * @param numUpgrade the num of the upgrade.
+     * @return the price of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public int getPriceWithNumUpgrade(String nameCharacter, int numSkill, String nameUpgrade, int numUpgrade) throws UpgradeSkillDataGetException{
         try{
             return Integer.parseInt(getObjectWithNumUpgrade(nameCharacter, numSkill, nameUpgrade, numUpgrade, ATTRIBUTE_STRING_PRICE));
@@ -234,6 +375,14 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get description.
+     * @param nameCharacter the name of a character.
+     * @param numSkill the num of a skill.
+     * @param nameUpgrade the name of an upgrade.
+     * @return the description of an upgrade.
+     * @throws UpgradeSkillDataGetException if the upgrade requested doesn't exist.
+     */
     public String getDescription(String nameCharacter, int numSkill, String nameUpgrade) throws UpgradeSkillDataGetException{
         try{
             return getObject(nameCharacter, numSkill, nameUpgrade, ATTRIBUTE_STRING_DESCRIPTION);
@@ -242,6 +391,10 @@ public class UpgradeSkillDBManager {
         }
     }
 
+    /**
+     * Get a list containing the main attribute name for the upgrade.
+     * @return a list containing the main attribute name for the upgrade.
+     */
     private List<String> listNameForGetting(){
         ArrayList<String> listName = new ArrayList<>();
         listName.add(ATTRIBUTE_STRING_NAME_CHARACTER);
@@ -250,6 +403,11 @@ public class UpgradeSkillDBManager {
         return listName;
     }
 
+    /**
+     * do a request for a select.
+     * @param nameCharacter the name of a character.
+     * @return a resultSet containing the result of a select for the UpgradeSkill table.
+     */
     private ResultSet selectUpgradeSkillForACharacter(String nameCharacter){
         ResultSet resultSet;
         resultSet = dbManager.selectIntoTable("SELECT * FROM UpgradeSkill WHERE nameCharacter = '" +
@@ -257,10 +415,16 @@ public class UpgradeSkillDBManager {
         return resultSet;
     }
 
+    /**
+     * Remove table UpgradeSkill.
+     */
     public void removeTableUpgradeSkill(){
         dbManager.dropTable(TABLE_NAME);
     }
 
+    /**
+     * Drop cascade.
+     */
     public void dropCascade(){
         dbManager.dropCascade();
     }
