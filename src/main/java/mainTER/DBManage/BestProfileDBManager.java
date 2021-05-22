953ffe3525163c0fa91aa22ad11230311cb0abce
anthony <anthony_pro1998@outlook.fr>
2021-05-22 12:46:38 +0200

    add Javadoc for BestProfile db.

--- src/main/java/mainTER/DBManage/BestProfileDBManager.java
+++ src/main/java/mainTER/DBManage/BestProfileDBManager.java
@@ -15,12 +15,22 @@ public class BestProfileDBManager {
 
     private static final String NAME_MAP_NAME = "mapName";
 
+    /**
+     * Constructor for the test purpose.
+     * @param nameFileDB the name of the databases for the test.
+     */
     public BestProfileDBManager(String nameFileDB){
         this.dbManager = new DBManager(nameFileDB,"test");
     }
 
+    /**
+     * Constructor for the application.
+     */
     public BestProfileDBManager(){this.dbManager = new DBManager(); }
 
+    /**
+     * Create table BestProfile who save the best profile for each map.
+     */
     public void createTableBestProfile(){
         ArrayList<String> listName = new ArrayList<>();
         ArrayList<Integer> listSize = new ArrayList<>();
@@ -30,11 +40,22 @@ public class BestProfileDBManager {
         dbManager.createTable(NAME_TABLE, listName, 0, listSize);
     }
 
+    /**
+     * verify if a best profile exist for a map.
+     * @param mapName the name of the map.
+     * @return true if a best profile exist for the map, false otherwise.
+     * @throws BestProfileDataGetException when there's problem with the table BestProfile.
+     */
     private boolean isBestProfileExist(String mapName) throws BestProfileDataGetException{
         List<String> listCharacter = getListNameFromDatabase();
         return listCharacter.contains(mapName);
     }
 
+    /**
+     * get a list containing all map from the table BestProfile.
+     * @return a list containing all map from the table BestProfile.
+     * @throws BestProfileDataGetException when there's a problem with the table BestProfile.
+     */
     public List<String> getListNameFromDatabase() throws BestProfileDataGetException{
         try{
             return dbManager.getList(NAME_TABLE, null, null, "mapName");
@@ -44,6 +65,13 @@ public class BestProfileDBManager {
         throw new BestProfileDataGetException();
     }
 
+    /**
+     * insert value into table BestProfile.
+     * @param name the name of the player.
+     * @param time the time he use to finish a map.
+     * @param mapName the name of the map he finished.
+     * @throws BestProfileDataExistAlreadyException when there's a problem with the table BestProfile.
+     */
     public void insertIntoTableBestProfile(String name, int time,String mapName) throws BestProfileDataExistAlreadyException {
         // TODO verify insert data
 
@@ -62,6 +90,11 @@ public class BestProfileDBManager {
         dbManager.insertIntoTable(NAME_TABLE, listInsert);
     }
 
+    /**
+     * get best time for a map from the databases.
+     * @param mapName the map we want to get the best time.
+     * @return the best time for a map from the databases.
+     */
     public int getTime(String mapName){
         ArrayList<String> listNameLine = new ArrayList<>();
         ArrayList<Object> listRealValueOfLine = new ArrayList<>();
@@ -73,6 +106,12 @@ public class BestProfileDBManager {
         }
 
     }
+
+    /**
+     * get the name of the best player for a map from the databases.
+     * @param mapName the map we want to get the name of the player who had the best time.
+     * @return the name of the best player for a map from the databases.
+     */
     public String getName(String mapName){
         ArrayList<String> listNameLine = new ArrayList<>();
         ArrayList<Object> listRealValueOfLine = new ArrayList<>();
@@ -86,6 +125,11 @@ public class BestProfileDBManager {
         return "";
     }
 
+    /**
+     * Change the best player for a map.
+     * @param name the new best player.
+     * @param mapName the map we want to change the best player.
+     */
     public void setName(String name,String mapName){
         ArrayList<String> listNameLine = new ArrayList<>();
         ArrayList<Object> listRealValueOfLine = new ArrayList<>();
@@ -93,6 +137,11 @@ public class BestProfileDBManager {
         dbManager.updateTable(NAME_TABLE, listNameLine, listRealValueOfLine, "name", name);
     }
 
+    /**
+     * Change the best time for a map.
+     * @param time the new best time.
+     * @param mapName the map we want to change the best time.
+     */
     public void setTime(int time,String mapName) {
         ArrayList<String> listNameLine = new ArrayList<>();
         ArrayList<Object> listRealValueOfLine = new ArrayList<>();
@@ -100,10 +149,16 @@ public class BestProfileDBManager {
         dbManager.updateTable(NAME_TABLE, listNameLine, listRealValueOfLine, "time", time);
     }
 
+    /**
+     * Delete the table BestProfile.
+     */
     public void removeTableProfile(){
         dbManager.dropTable(NAME_TABLE);
     }
 
+    /**
+     * Drop cascade.
+     */
     public void dropCascade(){
         dbManager.dropCascade();
     }
