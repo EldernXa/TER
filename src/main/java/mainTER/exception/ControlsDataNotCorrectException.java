6a4c0531958b20960b8e3864ca369469f544b90a
anthony <anthony_pro1998@outlook.fr>
2021-05-22 15:31:00 +0200

    add Javadoc for exception ControlsDataNotCorrectException.

--- src/main/java/mainTER/exception/ControlsDataNotCorrectException.java
+++ src/main/java/mainTER/exception/ControlsDataNotCorrectException.java
@@ -1,5 +1,8 @@
 package mainTER.exception;
 
+/**
+ * Exception for when the inserted values for the table controls aren't correct.
+ */
 public class ControlsDataNotCorrectException extends Exception{
 
     private final String message;
