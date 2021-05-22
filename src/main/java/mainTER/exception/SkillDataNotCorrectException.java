9fff562e374ccdcae64124ef5a3351a5703d49bf
anthony <anthony_pro1998@outlook.fr>
2021-05-22 15:53:50 +0200

    add Javadoc for exception SkillDataNotCorrectException.

--- src/main/java/mainTER/exception/SkillDataNotCorrectException.java
+++ src/main/java/mainTER/exception/SkillDataNotCorrectException.java
@@ -1,5 +1,8 @@
 package mainTER.exception;
 
+/**
+ * Exception for when we try to insert data that aren't correct into the table Skill.
+ */
 public class SkillDataNotCorrectException extends Exception{
 
     private final String message;
